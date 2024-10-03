0d07a6b3e138e1c379a22465bf84a01356707ddb
Dendi <pingqian1991@gmail.com>
2024-10-03 22:34:37 +0800

    fix: sponsor in mobile & ui bug

--- src/component/LogoList1.tsx
+++ src/component/LogoList1.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const LogoList1 = () => {
   return (
     <Row gutter={[16, 12]} className="px-5 mt-6 md:hidden">
-      <Col span={8}>
+      <Col span={8} className="flex flex-col justify-center">
         <Image
           src="/banner/sponsors/image3.svg"
           width={136}
@@ -14,7 +14,7 @@ const LogoList1 = () => {
         />
       </Col>
 
-      <Col span={8}>
+      <Col span={8} className="flex flex-col justify-center">
         <Image
           src="/banner/sponsors/image12.svg"
           width={136}
@@ -24,7 +24,7 @@ const LogoList1 = () => {
         />
       </Col>
 
-      <Col span={8}>
+      <Col span={8} className="flex flex-col justify-center">
         <Image
           src="/banner/sponsors/image4.svg"
           width={136}
@@ -33,7 +33,7 @@ const LogoList1 = () => {
           className="w-[100px] md:w-[136px]"
         />
       </Col>
-      <Col span={8}>
+      <Col span={8} className="flex flex-col justify-center">
         <Image
           src="/banner/sponsors/image1.png"
           width={136}
@@ -43,8 +43,7 @@ const LogoList1 = () => {
         />
       </Col>
 
-
-      <Col span={8}>
+      <Col span={8} className="flex flex-col justify-center">
         <Image
           src="/banner/sponsors/image7.svg"
           width={136}
@@ -54,7 +53,7 @@ const LogoList1 = () => {
         />
       </Col>
 
-      <Col span={8}>
+      <Col span={8} className="flex flex-col justify-center">
         <Image
           src="/banner/sponsors/image10.svg"
           width={136}
@@ -64,7 +63,7 @@ const LogoList1 = () => {
         />
       </Col>
 
-      <Col span={8}>
+      <Col span={8} className="flex flex-col justify-center">
         <Image
           src="/banner/sponsors/image8.svg"
           width={136}
@@ -73,7 +72,7 @@ const LogoList1 = () => {
           className="w-[100px] md:w-[136px]"
         />
       </Col>
-      <Col span={8}>
+      <Col span={8} className="flex flex-col justify-center">
         <Image
           src="/banner/sponsors/image13.svg"
           width={136}
@@ -82,7 +81,7 @@ const LogoList1 = () => {
           className="w-[100px] md:w-[136px]"
         />
       </Col>
-      <Col span={8}>
+      <Col span={8} className="flex flex-col justify-center">
         <Image
           src="/banner/sponsors/image11.svg"
           width={136}
@@ -90,9 +89,28 @@ const LogoList1 = () => {
           alt="icon"
           className="w-[100px] md:w-[136px]"
         />
-
       </Col>
-      <Col span={8}>
+      <Col span={8} className="flex flex-col justify-center">
+        <Image
+          src="/banner/sponsors/starkware.svg"
+          width={136}
+          height={48}
+          alt="icon"
+          className="w-[100px] md:w-[136px]"
+        />
+      </Col>
+
+      <Col span={8} className="flex flex-col justify-center">
+        <Image
+          src="/banner/sponsors/orbiter.svg"
+          width={136}
+          height={48}
+          alt="icon"
+          className="w-[100px] md:w-[136px]"
+        />
+      </Col>
+
+      <Col span={8} className="flex flex-col justify-center">
         <Image
           src="/banner/sponsors/image9.png"
           width={136}
